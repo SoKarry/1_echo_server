b5435696c81391a1c2c6aa1e5e84b7e406facebf
SoKarry <oleg29d@mail.com>
2021-03-19 16:56:51 +0300

    Сделан доп. 11 - многопользовательский чат

---
+++ chat_server.py
@@ -0,0 +1,37 @@
+import socket
+
+host = '127.0.0.1'
+port = 9091
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+s.bind((host, port))
+print('Сервер запущен успешно')
+
+users = {}
+
+while True:
+    data, addr = s.recvfrom(1024)
+
+    if data == '':
+        continue
+
+    user_id = addr[1]
+    data = data.decode()
+    if data == '__join':
+        print(f'Client {user_id} joined chat')
+        continue
+
+    if '<nick_check>' in data:
+        user_nick = data.split('>=')[1]
+        if user_nick not in users.values():
+            users.setdefault(addr, user_nick)
+            s.sendto('<nick_check_true>'.encode(), addr)
+            continue
+        else:
+            s.sendto('<nick_check_false>'.encode(), addr)
+            continue
+
+    data = f'{users.get(addr)}: {data}'
+    for user in users:
+        if user != addr:
+            s.sendto(data.encode(), user)
